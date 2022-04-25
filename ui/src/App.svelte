b645e84aa83a6a25eecbdbde0be0664e0f727c48
tejzpr <tejzpr@gmail.com>
2022-04-25 08:38:20 -0700

    Update

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -19,16 +19,25 @@
 	}
 
 	let selected = 'all';
-	
+	let loading = true;
 	function onChange(event) {
 		selected = event.currentTarget.value;
 	}
+
 
 	onMount(async () => {
 
 		store.subscribe(currentMessage => {
 			if (currentMessage.length > 0) {
-				monitorData = JSON.parse(currentMessage);
+				if (loading == true) {
+					loading = false;
+				}
+				try {
+					monitorData = JSON.parse(currentMessage);
+				} catch (err) {
+					postError(err);
+				}
+				
 			}
 		})
 
@@ -57,48 +66,68 @@
 		{notifyerror}
 	</div>
 	{/if}
-	<div class="content">
-		<div class="container body-main">
-			<div class="form-check">
-				<input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="all" on:change={onChange} checked={selected==='all'}>
-				<label class="form-check-label" for="inlineRadio1">View All Monitors</label>
-			  </div>
-			  <div class="form-check">
-				<input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="ok" on:change={onChange} checked={selected==='ok'}>
-				<label class="form-check-label" for="inlineRadio2">View Monitors that have status <span class="badge bg-success rounded-pill">OK</span> </label>
-			  </div>
-			  <div class="form-check">
-				<input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="error" on:change={onChange} checked={selected==='error'}>
-				<label class="form-check-label" for="inlineRadio3">View Monitors that have status <span class="badge bg-danger rounded-pill">ERROR</span></label>
-			  </div>
-		</div>
-		<div class="container body-main">
-			<ol class="list-group list-group-numbered">
-				{#each monitors as monitor}
-				
-					{#if (typeof monitorData[monitor.name] === 'undefined' ? "Loading" : monitorData[monitor.name]["status"] === "OK") && (selected === 'ok' || selected === 'all')}
-						<li class="list-group-item d-flex justify-content-between align-items-start">
-							<div class="ms-2 me-auto">
-								<div class="fw-bold">{monitor.name}</div>
-								{monitor.description}
+	{#if loading == true}
+		<div class="content">
+			<div class="container-fluid body-main">
+				<div class="row flex-xl-nowrap">
+					<div class="col-12">
+						<div class="d-flex justify-content-center">
+							<div class="spinner-border" role="status">
+								<span class="sr-only"></span>
 							</div>
-							<span class="badge monitor bg-success rounded-pill"></span>
-						</li>
-					{:else if (typeof monitorData[monitor.name] === 'undefined' ? "Loading" : monitorData[monitor.name]["status"] === "ERROR") && (selected === 'error' || selected === 'all')}
-						<li class="list-group-item d-flex justify-content-between align-items-start">
-							<div class="ms-2 me-auto">
-								<div class="fw-bold">{monitor.name}</div>
-								{monitor.description}
-							</div>
-							<span class="badge monitor bg-danger rounded-pill"></span>
-						</li>
+						</div>
+						<div class="d-flex loading justify-content-center">
+							Loading
+						</div>	
+					</div>
+				</div>
+			</div>
+		</div>	
+	{/if}
+	{#if loading == false}
+		<div class="content">
+			<div class="container-fluid body-main">
+				<div class="form-check">
+					<input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="all" on:change={onChange} checked={selected==='all'}>
+					<label class="form-check-label" for="inlineRadio1">View All Monitors</label>
+				</div>
+				<div class="form-check">
+					<input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="ok" on:change={onChange} checked={selected==='ok'}>
+					<label class="form-check-label" for="inlineRadio2">View Monitors that have status <span class="badge bg-success rounded-pill">OK</span> </label>
+				</div>
+				<div class="form-check">
+					<input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="error" on:change={onChange} checked={selected==='error'}>
+					<label class="form-check-label" for="inlineRadio3">View Monitors that have status <span class="badge bg-danger rounded-pill">ERROR</span></label>
+				</div>
+			</div>
+			<div class="container body-main">
+				<ol class="list-group list-group-numbered">
+					{#each monitors as monitor}
+					
+						{#if (typeof monitorData[monitor.name] === 'undefined' ? "Loading" : monitorData[monitor.name]["status"] === "OK") && (selected === 'ok' || selected === 'all')}
+							<li class="list-group-item d-flex justify-content-between align-items-start">
+								<div class="ms-2 me-auto">
+									<div class="fw-bold">{monitor.name}</div>
+									{monitor.description}
+								</div>
+								<span class="badge monitor bg-success rounded-pill"></span>
+							</li>
+						{:else if (typeof monitorData[monitor.name] === 'undefined' ? "Loading" : monitorData[monitor.name]["status"] === "ERROR") && (selected === 'error' || selected === 'all')}
+							<li class="list-group-item d-flex justify-content-between align-items-start">
+								<div class="ms-2 me-auto">
+									<div class="fw-bold">{monitor.name}</div>
+									{monitor.description}
+								</div>
+								<span class="badge monitor bg-danger rounded-pill"></span>
+							</li>
 
-					{/if}
-				
-				{/each}
-			</ol>
-		</div>
-	</div>	
+						{/if}
+					
+					{/each}
+				</ol>
+			</div>
+		</div>	
+	{/if}
 </main>
 
 
@@ -110,6 +139,9 @@
 		display: inline-flex;
 		align-items: center;
     	justify-content: center;
+	}
+	.loading {
+		margin-top:20px;
 	}
 	.body-main {
 		margin-top:20px;
--- ui/src/Header.svelte
+++ ui/src/Header.svelte
@@ -11,7 +11,7 @@
 
 
 <header class="p-3 bg-dark text-white">
-    <div class="container">
+    <div class="container-fluid">
         <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
         <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
             <img src="/static/images/logo.png" width="40" height="32" alt="Monito" title="Monito"/> 
